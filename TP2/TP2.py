7f8276f99d30a70c5ac9fe7d138ac81154a148b1
Ignacio <51163669+Igna138@users.noreply.github.com>
2023-06-09 19:03:25 -0300

    Actualizar TP2.py

--- TP2/TP2.py
+++ TP2/TP2.py
@@ -1,37 +1,63 @@
 FD = "peajes.txt"
 
-#Consigna 1
+# Consigna 1
 def idioma():
     pass
 
-#Consigna 2
+# Consigna 2
 def veh_proc():
     pass
 
-#Consigna 3
+# Consigna 3
 def imp_tot():
     pass
 
-#Consigna 4
+# Consigna 4
 def cant_patente():
     pass
 
-#Consigna 5
+# Consigna 5
 def import_total_final():
     pass
 
-#Consigna 6
+# Consigna 6
 def otro_pais():
     pass
 
-#Consigna 7
+# Consigna 7
 def dist_prom():
     pass
 
-#Programa principal
+# Programa principal
 def test():
-    #Aqui se muestran los resultados y prints del script
-    pass
+    # Aqui se muestran los resultados y prints del script
+    
 
+    # Visualizacion de resultados...
+    print('(r1) - Idioma a usar en los informes: ', idioma)
+
+    print()
+    print('(r2) - Cantidad de patentes de Argentina:', carg)
+    print('(r2) - Cantidad de patentes de Bolivia:', cbol)
+    print('(r2) - Cantidad de patentes de Brasil:', cbra)
+    print('(r2) - Cantidad de patentes de Chile:', cchi)
+    print('(r2) - Cantidad de patentes de Paraguay:', cpar)
+    print('(r2) - Cantidad de patentes de Uruguay:', curu)
+    print('(r2) - Cantidad de patentes de otro pais;', cotr)
+
+    print()
+    print('(r3) - Importe acumulado total de importes finales: ', imp_acu_total)
+
+    print()
+    print('(r4) - Primera patente del archivo: ', primera, '- Frecuencia de aparicion: ', cpp)
+
+    print ()
+    print('(r5) - Mayor importe final cobrado:', mayimp, ' - Patente a la que se cobró ese importe: ', maypat)
+
+
+
+
+
+Cantidad de patentes de otro pais;', cotr)
 if __name__ == "__main__":
     test()
