1ee5bd180cfce14a7be4d78c2eb74d12c23f2c09
Igna138 <ignacio.blanco.9892@gmail.com>
2023-06-09 00:24:10 -0300

    Inicio del TP2

---
+++ TP2/TP2.py
@@ -0,0 +1,37 @@
+FD = "peajes.txt"
+
+#Consigna 1
+def idioma():
+    pass
+
+#Consigna 2
+def veh_proc():
+    pass
+
+#Consigna 3
+def imp_tot():
+    pass
+
+#Consigna 4
+def cant_patente():
+    pass
+
+#Consigna 5
+def import_total_final():
+    pass
+
+#Consigna 6
+def otro_pais():
+    pass
+
+#Consigna 7
+def dist_prom():
+    pass
+
+#Programa principal
+def test():
+    #Aqui se muestran los resultados y prints del script
+    pass
+
+if __name__ == "__main__":
+    test()
