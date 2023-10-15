5acf8f7936666d6d8d681686ee96317525bfe3e4
Nacho <ignacio.blanco.9892@gmail.com>
2023-10-15 02:41:24 -0300

    Puntos 1, 2 y 3 validados

--- TP4/principal.py
+++ TP4/principal.py
@@ -1,15 +1,10 @@
-import os.path
-
 from ticket import *
 
 def principal():
     opcion = -1
     flag_1 = False
-    archivo_csv= "peajes-tp4.csv"
-    vector_teclado=[]
-    #vp = cargar_archivo_csv(archivo_csv)
-    bin = "peajes.dat"
-
+    archivo_csv = "peajes-tp4.csv"
+    bin_arch = "peajes.dat"
 
     while opcion != 0:
         mostrar_menu()
@@ -17,45 +12,47 @@ def principal():
 
         if opcion == 1:
             if not flag_1:
-                generar_archivo_b(archivo_csv, bin)
+                generar_archivo_b(archivo_csv, bin_arch)
                 flag_1 = True
                 print("Archivo creado con exito!!")
             else:
                 n = input("Esta seguro de crear de nuevo el archivo, se perdera todo lo anterior.. Responda (SI o NO):")
-                if n.lower == "si":
-                    generar_archivo_b(archivo_csv, bin)
-                    break
+                if n.lower() == "si":
+                    generar_archivo_b(archivo_csv, bin_arch)
+                    print("El archivo cambio !")
                 else:
-                    print("Ok, queda como antes..") #Revisar este print
+                    print("Ok, queda como antes..")
+
 
         elif opcion == 2:
-            v =  generarTicket()
-            vector_teclado = crear_registro_p2(v)
-            agregar_teclado_a_binario(vector_teclado, bin)
+            a = generarTicket()
+            agregar_teclado_a_binario(a, bin_arch)
 
         elif opcion == 3:
-            mostrar_datos_binario(bin) #Revisar
+            mostrar_datos_binario(bin_arch) #Revisar
 
         elif opcion == 4:
-            #p = validar_patente_p4() Revisar validacion
-            p = input("ingrese una patente a buscar: ")
-            punto4(bin, p)
-
-
+            #p = validat_patente_p4()
+            pat = input('Ingrese la patente: ')
+            punto4(bin_arch, pat)
 
         elif opcion == 5:
             print('Nro de ticket: ')
-            #c = validate_intervalo(1, 999999999) Revisar validaciones
-            c = input("Ingrese codigo a buscar: ")
-            punto_5(bin, c)
+            c = validate_intervalo(1, 999999999)
+            punto_5(bin_arch, c)
+
         elif opcion == 6:
-            pass
+            mat = cargar_matriz(bin_arch)
+            mostrar_matriz(mat)
 
         elif opcion == 7:
-            pass
+            mat = cargar_matriz(bin_arch)
+            punto_7(mat)
 
         elif opcion == 8:
-            pass
+            prom = calculo_promedio(bin_arch)
+            v = punto_8(bin_arch, prom)
+            mostrar_vector(v, prom)
 
         elif opcion == 0:
             print("-----FIN DEL PROGAMA-----")
